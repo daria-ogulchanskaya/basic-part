efab1b1dc7c9571c4951e3d8e693a856a9b66100
Daria Ogulchanskaya <55715903+daria-ogulchanskaya@users.noreply.github.com>
2020-04-15 11:02:13 +0300

    Updated CREATE TABLE.

--- CREATE TABLE.sql
+++ CREATE TABLE.sql
@@ -1,33 +1,33 @@
-CREATE TABLE Flower (
+CREATE TABLE [Flower] (
 	[Id]   INT IDENTITY PRIMARY KEY,
 	[Name] VARCHAR(255) NOT NULL,
 );
 
-CREATE TABLE Plantation (
+CREATE TABLE [Plantation] (
 	[Id]	  INT IDENTITY PRIMARY KEY,
 	[Name]	  VARCHAR(255) NOT NULL,
 	[Address] VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE PlantationFlowers (
+CREATE TABLE [PlantationFlowers] (
 	[PlantationId] INT NOT NULL FOREIGN KEY REFERENCES Plantation(Id),
 	[FlowerId]     INT NOT NULL FOREIGN KEY REFERENCES Flower(Id),
 	[Amount]       INT NOT NULL CHECK([Amount] > 0)
 );
 
-CREATE TABLE Warehouse (
+CREATE TABLE [Warehouse] (
 	[Id]      INT IDENTITY PRIMARY KEY,
 	[Name]    VARCHAR(255) NOT NULL,
 	[Address] VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE WarehouseFlowers (
+CREATE TABLE [WarehouseFlowers] (
 	[WarehouseId] INT NOT NULL FOREIGN KEY REFERENCES Warehouse(Id),
 	[FlowerId]    INT NOT NULL FOREIGN KEY REFERENCES Flower(Id),
 	[Amount]      INT NOT NULL CHECK([Amount] > 0)
 );
 
-CREATE TABLE Supply (
+CREATE TABLE [Supply] (
 	[Id]	       INT IDENTITY PRIMARY KEY,
 	[PlantationId] INT NOT NULL FOREIGN KEY REFERENCES Plantation(Id),
 	[WarehouseId]  INT NOT NULL FOREIGN KEY REFERENCES Warehouse(Id),
@@ -36,7 +36,7 @@ CREATE TABLE Supply (
 	[Status]       VARCHAR(10) NOT NULL CHECK([Status] IN ('Scheduled', 'InProgress', 'Canceled', 'Closed'))
 );
 
-CREATE TABLE SupplyFlowers (
+CREATE TABLE [SupplyFlowers] (
 	[SupplyId] INT NOT NULL FOREIGN KEY REFERENCES Supply(Id),
 	[FlowerId] INT NOT NULL FOREIGN KEY REFERENCES Flower(Id),
 	[Amount]   INT NOT NULL CHECK([Amount] > 0)
