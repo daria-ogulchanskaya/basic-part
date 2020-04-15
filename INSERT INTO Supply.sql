41e58e3e2039e3e57d41a3f947fbe58364182600
Daria Ogulchanskaya <55715903+daria-ogulchanskaya@users.noreply.github.com>
2020-04-15 10:59:22 +0300

    Updated INSERT INTO Supply.

--- INSERT INTO Supply.sql
+++ INSERT INTO Supply.sql
@@ -1,12 +1,12 @@
 INSERT INTO [Supply] ([PlantationId], [WarehouseId], [ScheduleDate], [ClosedDate], [Status])
 VALUES 
 	(4, 1, '2020-07-11', '2020-07-30', 'Closed'), 
-	(5,	2, '2020-08-15', '2020-08-31', 'Closed'),
+	(5, 2, '2020-08-15', '2020-08-31', 'Closed'),
 	(3, 3, '2020-04-23', '2020-05-6',  'Closed'),
 	(3, 3, '2020-03-16', '2020-04-16', 'Closed'),
-	(1, 2, '2020-01-20', NULL,		   'InProgress'), 
-	(2,	3, '2020-02-5',  NULL,		   'InProgress'),
-	(3, 3, '2020-03-16', NULL,		   'InProgress')
+	(1, 2, '2020-01-20', NULL,	   'InProgress'), 
+	(2, 3, '2020-02-5',  NULL,	   'InProgress'),
+	(3, 3, '2020-03-16', NULL,	   'InProgress')
 
 INSERT INTO [SupplyFlowers] 
 		   ([SupplyId]
